4a491abf98b1bd53fd0ca3f567837e9f8bfcabf7
takahirom <takam.dev@gmail.com>
2014-10-23 10:24:12 +0900

    first commit

---
+++ apk_installer.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+import subprocess
+import time
+import threading
+import select
+from fcntl import fcntl, F_GETFL, F_SETFL
+from os import O_NONBLOCK, read
+import tempfile
+
+import os
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.install, cancel=self.dismiss_popup)
+        self._popup = Popup(
+            title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def install(self, path, filename):
+        self.dismiss_popup()
+        print os.path.join(path, filename[0])
+        threading.Thread(
+            target=self.install_and_loop, args=[os.path.join(path, filename[0])]).start()
+
+    def install_and_loop(self, path):
+        print(os.path.dirname(os.path.realpath(__file__)) +
+              "/adb install -r " + path)
+        f = tempfile.TemporaryFile()
+        proc = subprocess.Popen(os.path.dirname(os.path.realpath(
+            __file__)) + "/adb install -r " + path, shell=True, stdout=f, stderr=f)
+        time.sleep(2)
+        proc.terminate()
+        # wait for the process to terminate otherwise the output is garbled
+        proc.wait()
+
+        # print saved output
+        f.seek(0)  # rewind to the beginning of the file
+        self.text_input.text = str(f.read())
+        f.close()
+
+
+class ApkInstaller(App):
+    pass
+
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
+
+if __name__ == '__main__':
+    ApkInstaller().run()
---
+++ apk_installer.spec
@@ -0,0 +1,34 @@
+# -*- mode: python -*-
+from kivy.tools.packaging.pyinstaller_hooks import install_hooks
+install_hooks(globals())
+
+block_cipher = None
+
+
+a = Analysis(['apk_installer.py'],
+             pathex=['/Users/takam/Documents/program/kivy'],
+             hiddenimports=[],
+             #hookspath=None,
+             runtime_hooks=None,
+             cipher=block_cipher)
+pyz = PYZ(a.pure,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='apk_installer',
+          debug=False,
+          strip=None,
+          upx=True,
+          console=False )
+coll = COLLECT(exe,
+               Tree('.'),
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=None,
+               upx=True,
+               name='apk_installer')
+app = BUNDLE(coll,
+             name='apk_installer.app',
+             icon=None)
